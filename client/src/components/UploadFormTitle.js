1dc08e2efb3cf0384abfae1615b164e26c8c9ab6
Vlad Stelmakh <vlad1k9369@gmail.com>
2021-08-21 22:21:38 +0300

    Fix bug

--- client/src/components/UploadFormTitle.js
+++ client/src/components/UploadFormTitle.js
@@ -1,4 +1,4 @@
-export const UploadFormTitle = () => {
+ const UploadFormTitle = () => {
     return (
         <div className="text-center">
             <h2 className="text-3xl font-bold text-gray-900">
@@ -7,3 +7,5 @@ export const UploadFormTitle = () => {
         </div>
     );
 };
+
+export default UploadFormTitle;

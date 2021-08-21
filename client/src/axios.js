54b2e59521885ae776870cb8947e2c24fb3912f0
Vlad Stelmakh <vlad1k9369@gmail.com>
2021-08-21 17:50:00 +0300

    set app url

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8080/",
+  baseURL: "http://109.248.175.110:8080/",
   headers: {"Access-Control-Allow-Origin": "*"}
 })
-//http://109.248.175.110:8080/
+//http://109.248.175.110:8080/ http://127.0.0.1:8080/
 export default axiosInstance
